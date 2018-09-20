24576104d85eea903bc2f08d0a5c46f2bc573bfe
abrahplaya <abrahamglasser@gmail.com>
2018-09-20 11:24:29 -0400

    added name/password prepending and concatenation

--- client.c
+++ client.c
@@ -38,6 +38,19 @@ char* deblank(char* input)
     return output;
 }
 
+// function for prepending strings
+void prepend(char* s, const char* t)
+{
+    size_t len = strlen(t);
+    size_t i;
+
+    memmove(s + len, s, strlen(s) + 1);
+
+    for (i = 0; i < len; ++i)
+    {
+        s[i] = t[i];
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -47,12 +60,19 @@ int main(int argc, char *argv[])
         printf("None of the 4 required arguments should contain spaces.\n");
     }
 
+    // prepend #name: to name
     char* name = argv[3];
+    prepend(name, "#name:");
+
+    // prepend #password: to password
     char* password = argv[4];
+    prepend(password, "#password:");
+
+    //concatenate name and password
+    strncat(name, password, (sizeof(name) - strlen(password)) );
 
     int fd = 0;
     char buff[1024];
-
 
     //Setup Buffer Array
     memset(buff, '0',sizeof(buff)); 
@@ -91,7 +111,8 @@ int main(int argc, char *argv[])
     // send name and password
     printf("Sending name and password...\n");
 
-    // copy the name char* into the char[]
+    // copy the name (which is the concatenated name & password)
+    // char* into the char[] for sending
     strncpy(buff, name, sizeof(buff - 1));
     buff[sizeof(buff)-1] = '\0';
 
@@ -116,38 +137,43 @@ int main(int argc, char *argv[])
     }
     printf("\nReceived FROM SERVER: %s ",buff);
 
-    // TODO WHY DOESNT THIS WORK HERE?
-    //if (read_server_response() == 0){return 0;}
-///////
-
-    // copy the password char* into the char[]
-    strncpy(buff, password, sizeof(buff - 1));
-    buff[sizeof(buff)-1] = '\0';
-
-    // send the char[] with the password
-    in = send(fd,buff,strlen(buff),0);
-    if (in < 0) 
-    {
-     perror("\nClient Error: Writing to Server");
-        return 0;
-    }
-
-//TODO
-///////
-    /* Now read server response */
-    //TRY TO REPLACE THIS WITH SERVER LISTENING FUNCTION AT THE BOTTOM
-    bzero(buff,256);
-    in = recv(fd,buff,255,0);
-    if (in < 0) 
-    {
-        perror("\nClient Error: Reading from Server");
-        return 0;
-    }
-    printf("\nReceived FROM SERVER: %s ",buff);
+//*****
+// dont need the below commented out code because we only need to
+// send the name which contains the name & password
+// (since we concatenated them into name)
 
     // TODO WHY DOESNT THIS WORK HERE?
     //if (read_server_response() == 0){return 0;}
 ///////
+
+//     // copy the password char* into the char[]
+//     strncpy(buff, password, sizeof(buff - 1));
+//     buff[sizeof(buff)-1] = '\0';
+
+//     // send the char[] with the password
+//     in = send(fd,buff,strlen(buff),0);
+//     if (in < 0) 
+//     {
+//      perror("\nClient Error: Writing to Server");
+//         return 0;
+//     }
+
+// //TODO
+// ///////
+//     /* Now read server response */
+//     //TRY TO REPLACE THIS WITH SERVER LISTENING FUNCTION AT THE BOTTOM
+//     bzero(buff,256);
+//     in = recv(fd,buff,255,0);
+//     if (in < 0) 
+//     {
+//         perror("\nClient Error: Reading from Server");
+//         return 0;
+//     }
+//     printf("\nReceived FROM SERVER: %s ",buff);
+
+//     // TODO WHY DOESNT THIS WORK HERE?
+//     //if (read_server_response() == 0){return 0;}
+// ///////
 
     // infinite loop to send and read in messages
     while(1)
